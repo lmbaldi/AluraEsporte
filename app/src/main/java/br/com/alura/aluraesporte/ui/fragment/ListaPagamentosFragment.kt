012da54539d134a9b3afbb35fa91235946baf84e
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-07-17 19:17:02 -0300

    implementing bottom navigation

--- app/src/main/java/br/com/alura/aluraesporte/ui/fragment/ListaPagamentosFragment.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/ListaPagamentosFragment.kt
@@ -9,15 +9,18 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import br.com.alura.aluraesporte.R
 import br.com.alura.aluraesporte.ui.recyclerview.adapter.ListaPagamentosAdapter
+import br.com.alura.aluraesporte.ui.viewmodel.EstadoAppViewModel
 import br.com.alura.aluraesporte.ui.viewmodel.PagamentoViewModel
 import kotlinx.android.synthetic.main.lista_pagamentos.*
 import org.koin.android.ext.android.inject
+import org.koin.android.viewmodel.ext.android.sharedViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class ListaPagamentosFragment : Fragment() {
 
     private val adapter: ListaPagamentosAdapter by inject()
     private val viewModel: PagamentoViewModel by viewModel()
+    private val estadoAppViewModel: EstadoAppViewModel by sharedViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,6 +33,7 @@ class ListaPagamentosFragment : Fragment() {
     @SuppressLint("FragmentLiveDataObserve")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        estadoAppViewModel.temAppBar = true
         lista_pagamentos_recyclerview.adapter = adapter
         viewModel.todos().observe( this, Observer{
             it?.let{ pagamentosEncontrados ->
